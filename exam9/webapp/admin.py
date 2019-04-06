364a40e24970a0c9af0de24b2b4af1e62fe2ec0d
Maria Sharshenova <maria@sharshenov.com>
2019-04-06 12:51:53 +0600

    Добавлены модели, настроено их отображение в админке

--- exam9/webapp/admin.py
+++ exam9/webapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from webapp.models import RegistrationToken
+from webapp.models import RegistrationToken, Category, Product, ProductPhoto, Order
 
 
 # Register your models here.
@@ -10,5 +10,16 @@ class RegistrationTokenAdmin(admin.ModelAdmin):
     list_display = ['pk', 'user', 'created_at']
     readonly_fields = ['token']
 
+
+def list_admin_with_pk(*fields):
+    class PkListAdmin(admin.ModelAdmin):
+        list_display = ['pk'] + list(fields)
+    return PkListAdmin
+
+
+admin.site.register(Category, list_admin_with_pk('name'))
+admin.site.register(Product, list_admin_with_pk('name'))
+admin.site.register(ProductPhoto, list_admin_with_pk('product'))
+admin.site.register(Order, list_admin_with_pk('user', 'products', 'phone', 'address'))
 
 admin.site.register(RegistrationToken, RegistrationTokenAdmin)
--- exam9/webapp/models.py
+++ exam9/webapp/models.py
@@ -23,3 +23,65 @@ class RegistrationToken(models.Model):
 
     def __str__(self):
         return "%s" % self.token
+
+
+
+class SoftDeleteManager(models.Manager):
+    def active(self):
+        return self.filter(is_deleted=False)
+
+    def deleted(self):
+        return self.filter(is_deleted=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    is_deleted = models.BooleanField(default=False)
+    objects = SoftDeleteManager()
+
+    def str(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    date = models.DateField(verbose_name='Дата')
+    category = models.ManyToManyField(Category, blank=True, related_name="products", verbose_name='Категория')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+    is_deleted = models.BooleanField(default=False)
+    objects = SoftDeleteManager()
+
+    def str(self):
+        return self.name
+
+
+class ProductPhoto(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="photos", verbose_name="Товар")
+    photo = models.ImageField(upload_to='posters', null=True, blank=True, verbose_name='Фото')
+    is_deleted = models.BooleanField(default=False)
+    objects = SoftDeleteManager()
+
+    class Meta:
+        verbose_name_plural = 'Photos'
+
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, blank=False, null=False, related_name="orders",
+                             verbose_name="Пользователь")
+    products = models.ManyToManyField(Product, related_name="orders", verbose_name="Товары")
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    address = models.CharField(max_length=40, verbose_name='Адрес')
+    comment = models.TextField(max_length=4000, null=True, blank=True, verbose_name='Комментарий')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def str(self):
+        return self.phone
